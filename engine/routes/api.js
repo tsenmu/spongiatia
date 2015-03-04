9033f49c0e14a3ca9245b10f86194a0bf7c87f22
Jingjie Zheng <i@tsenmu.com>
2015-03-04 16:06:19 -0500

    no message

--- engine/routes/api.js
+++ engine/routes/api.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 
 router.get('/search', function(req, res, next) {
   var params = req.params;
-  res.json(params)
+  var query = params.q;
+  console.log (params)
+  var requestCallback = function (error, response, body) {
+    res.json(body);
+  }
+  var uri = 'http://api.stackexchange.com/2.2/search/advanced' +
+    '?order=desc' +
+    '&sort=activity' +
+    '&site=stackoverflow' +
+    '&q=' + query;
+  request(uri, requestCallback);
 });
 
 module.exports = router;
