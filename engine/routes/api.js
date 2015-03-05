daf5d9d7c2b56bb69e2535ca8a4ea1c94881cd44
Jingjie Zheng <i@tsenmu.com>
2015-03-05 00:37:42 -0500

    save working status

--- engine/routes/api.js
+++ engine/routes/api.js
@@ -1,24 +1,50 @@
+'use strict';
+
 var express = require('express');
 var request = require('request');
+var _ = require('lodash');
 var router = express.Router();
+
+var stackOverflowUrl = 'http://api.stackexchange.com/2.2';
 
 router.get('/search/', function(req, res, next) {
   var query = req.query;
   var q = query.q;
-  var requestCallback = function (error, response, body) {
-    console.log(body);
-    res.json(body);
+  var questionIds = [];
+  var questionRequestCallback = function (error, response, body) {
+    var items = body.items;
   }
-  var uri = 'http://api.stackexchange.com/2.2/search/advanced' +
-    '?order=desc' +
-    '&sort=activity' +
-    '&site=stackoverflow' +
+
+  var searchRequestCallback = function (error, response, body) {
+    var items = body.items;
+    _.forEach(items, function(item) {
+      questionIds.push(item.question_id);
+    });
+
+    _.forEach(questionIds, function(questionId) {
+      var questionUri = stackOverflowUrl + '/questions/' + questionId + '?' +
+      '&site=stackoverflow' + 
+      '&sort=votes';
+      request({
+        uri: questionUri,
+        gzip: true,
+        json: true
+      }, questionRequestCallback);
+    });
+    res.json(items);
+  }
+  var searchUri = stackOverflowUrl + '/search/advanced?' + 
+    '&site=stackoverflow' + 
+    '&order=desc' +
+    '&sort=relevance' +
+    '&accepted=True' +
+    '&pagesize=10' +
     '&q=' + q;
   request({
-    uri: uri,
+    uri: searchUri,
     gzip: true,
     json: true
-  }, requestCallback);
+  }, searchRequestCallback);
 });
 
 module.exports = router;
